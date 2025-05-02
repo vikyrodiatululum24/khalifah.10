91458d3880e382dc4969ff813b9c4bbe94114d88
vickyrodiatululum <vickyrodiatululum24@gmail.com>
2025-05-02 12:46:58 +0700

    update tailiwnd

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    server: {
-        host: "0.0.0.0", // biar bisa diakses dari jaringan
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            host: "192.168.1.9", // GANTI dengan IP lokal kamu
-        },
-    },
+    // server: {
+    //     host: "0.0.0.0", // biar bisa diakses dari jaringan
+    //     port: 5173,
+    //     strictPort: true,
+    //     hmr: {
+    //         host: "192.168.1.9", // GANTI dengan IP lokal kamu
+    //     },
+    // },
     plugins: [
         tailwindcss(),
     ],
